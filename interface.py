b401ff49404b4bbd46d14098f37d3e1f93a50564
kuropon1984 <66627796+kuropon1984@users.noreply.github.com>
2020-11-01 17:43:49 -0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,29 @@
+from tkinter import *
+raiz=Tk()
+raiz.title("profundidades")
+raiz.iconbitmap("escafandra.ico")
+
+opciones=Frame(raiz, width=100, height=100)
+opciones.pack()
+
+titulo=Label(opciones,text="Busqueda profunda",pady=50, padx=50)
+titulo.grid(row=0,column=0)
+
+class boton():
+    def __init__(self,texto,pocicion):
+        self.texto=texto
+        self.pocicion=pocicion
+    def desplega(self):
+        self.texto=Button(opciones,text=self.texto)
+        self.texto.grid(row=self.pocicion, column=0, pady=5, padx=5)
+
+
+tutorial=boton("tutorial",1)
+tutorial.desplega()
+
+dosj=boton("dos jugadores",2)
+dosj.desplega()
+tresj=boton("tres jugadores",3)
+tresj.desplega()
+
+raiz.mainloop()
